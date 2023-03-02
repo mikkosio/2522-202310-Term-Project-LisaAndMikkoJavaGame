f0f601bf4ffbc35fd2e345b53aa394c2911a8498
Mikko Sio <msio1@my.bcit.ca>
2023-03-02 15:17:03 -0800

    Initial commit

---
+++ src/main/java/ca/bcit/comp2522/termproject/lisaandmikkojavagame/HelloController.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2522.termproject.lisaandmikkojavagame;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.bcit.comp2522.termproject.lisaandmikkojavagame {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens ca.bcit.comp2522.termproject.lisaandmikkojavagame to javafx.fxml;
+    exports ca.bcit.comp2522.termproject.lisaandmikkojavagame;
+}
